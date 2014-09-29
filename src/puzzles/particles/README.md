f908c8cfefffd8d0126988e42ba3b9461eff4ee6
Jazmine James <jazminemichelle@gmail.com>
2014-09-28 23:10:34 -0400

    finish animation puzzle, tests and doc

--- src/puzzles/particles/README.md
+++ src/puzzles/particles/README.md
@@ -18,4 +18,6 @@ The solution makes the following assumptions about the input values:
 
 ## Algorithm
 
-TODO
+We can think of the movement of leftward and rightward particles separately. Picture two arrays of 'X' and '.', where an 'X' represents a particle moving in that direction and a '.' represents the absence of a particle.
+
+We can more easily represent the 'X's and '.'s as 0s and 1s. Furthermore, an array of 0s and 1s is simply a binary representation of a number. We can model the movement of leftward and rightward particles by performing bitwise operations on these numbers. We can also check the 'animation' of the tube at any time by performing a bitwise or on the leftward and rightward numeric representations, since a bitwise or will check whether any type of particle is in each position.
--- test/puzzles/particles/AnimationTest.java
+++ test/puzzles/particles/AnimationTest.java
@@ -1,5 +1,6 @@
 package puzzles.particles;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -7,8 +8,140 @@ import org.junit.Test;
 public class AnimationTest {
 
     @Test
-    public void stringToBinaryTest() {
-
+    public void stringToBinaryWithEmptyInputTest() {
+        String input = "";
+        long result = Animation.stringToBinary(input, Animation.Direction.left);
+        assertEquals(0L, result);
     }
 
+    @Test
+    public void stringToBinaryWithOneDirectionTest1() {
+        String input = "..R....";
+        long result = Animation.stringToBinary(input, Animation.Direction.left);
+        assertEquals(0L, result);
+    }
+
+    @Test
+    public void stringToBinaryWithOneDirectionTest2() {
+        String input = "......R";
+        long result = Animation.stringToBinary(input, Animation.Direction.right);
+        assertEquals(1L, result);
+    }
+
+    @Test
+    public void stringToBinaryTest1() {
+        String input = "RR..LRL";
+        long result = Animation.stringToBinary(input, Animation.Direction.left);
+        long expected = 0b101;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void stringToBinaryTest2() {
+        String input = "RR..LRL";
+        long result = Animation.stringToBinary(input, Animation.Direction.right);
+        long expected = 0b1100010;
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void animateLineTest1() {
+        long chamber = 0b10010110;
+        String expected = "X..X.XX.";
+        String result = Animation.animateLine(chamber, 8);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void animateLineTest2() {
+        long chamber = 0b0110;
+        String expected = ".....XX.";
+        String result = Animation.animateLine(chamber, 8);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void animateLineTest3() {
+        long chamber = 0L;
+        String expected = "........";
+        String result = Animation.animateLine(chamber, 8);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void animateTest1() {
+        String[] result = Animation.animate(2, "..R....");
+        String[] expected =
+                {"..X....",
+                 "....X..",
+                 "......X",
+                 "......." };
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void animateTest2() {
+        String[] result = Animation.animate(3, "RR..LRL");
+        String[] expected =
+                {"XX..XXX",
+                 ".X.XX..",
+                 "X.....X",
+                 "......."};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void animateTest3() {
+        String[] result = Animation.animate(2, "LRLR.LRLR");
+        String[] expected =
+                {"XXXX.XXXX",
+                 "X..X.X..X",
+                 ".X.X.X.X.",
+                 ".X.....X.",
+                 "........."};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void animateTest4() {
+        String[] result = Animation.animate(10, "RLRLRLRLRL");
+        String[] expected =
+                {"XXXXXXXXXX",
+                 ".........."};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void animateTest5() {
+        String[] result = Animation.animate(1, "...");
+        String[] expected =
+                {"..."};
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void animateTest6() {
+        String[] result = Animation.animate(1, "LRRL.LR.LRR.R.LRRL.");
+        String[] expected =
+                {"XXXX.XX.XXX.X.XXXX.",
+                "..XXX..X..XX.X..XX.",
+                ".X.XX.X.X..XX.XX.XX",
+                "X.X.XX...X.XXXXX..X",
+                ".X..XXX...X..XX.X..",
+                "X..X..XX.X.XX.XX.X.",
+                "..X....XX..XX..XX.X",
+                ".X.....XXXX..X..XX.",
+                "X.....X..XX...X..XX",
+                ".....X..X.XX...X..X",
+                "....X..X...XX...X..",
+                "...X..X.....XX...X.",
+                "..X..X.......XX...X",
+                ".X..X.........XX...",
+                "X..X...........XX..",
+                "..X.............XX.",
+                ".X...............XX",
+                "X.................X",
+                "..................."};
+        assertArrayEquals(expected, result);
+    }
 }
